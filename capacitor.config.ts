af4d01bab5c73893084e35ce49b3c4f2819351a2
Rascol Radu <rascol.radu92@gmail.com>
2024-09-02 17:01:57 +0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'snapzz',
+  webDir: 'www'
+};
+
+export default config;
