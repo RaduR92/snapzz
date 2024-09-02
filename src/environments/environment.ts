b1ea59e7e098956546ae211433b36b80a9d02d79
Rascol Radu <rascol.radu92@gmail.com>
2024-09-02 17:41:16 +0300

    Updates

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyDDM9DCGri9LmH0KcI6vI6RSGKcRateqSg",
+    authDomain: "snapzz-fff39.firebaseapp.com",
+    projectId: "snapzz-fff39",
+    storageBucket: "snapzz-fff39.appspot.com",
+    messagingSenderId: "357088362818",
+    appId: "1:357088362818:web:99e83e86f3f35f708d88db"
+  }
 };
 
 /*
