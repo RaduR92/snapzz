511afd1171cdf172c68b2495112eb5e4d3cc6f30
Rascol Radu <rascol.radu92@gmail.com>
2024-09-03 23:43:58 +0300

    Add login page Add register page Add forgot password page Add dashboard page

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyDDM9DCGri9LmH0KcI6vI6RSGKcRateqSg",
+    authDomain: "snapzz-fff39.firebaseapp.com",
+    projectId: "snapzz-fff39",
+    storageBucket: "snapzz-fff39.appspot.com",
+    messagingSenderId: "357088362818",
+    appId: "1:357088362818:web:99e83e86f3f35f708d88db"
+  }
 };
